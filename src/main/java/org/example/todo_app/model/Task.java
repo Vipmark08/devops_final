d9147c5482224e6370d70da460e639a8ca3bc035
Your Name <you@example.com>
2025-12-18 20:29:44 +0500

    fix: change title in develop branch

--- src/main/java/org/example/todo_app/model/Task.java
+++ src/main/java/org/example/todo_app/model/Task.java
@@ -17,7 +17,7 @@ public class Task {
     private Long id;
 
     @Column(nullable = false)
-    private String title;
+    private String title = "from-develop";
 
     private String description;
 
